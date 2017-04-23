0547497a0e0ed154445b7a96920866af4cef174e
jade <jadealvares@hotmail.co.uk>
2017-04-23 19:27:05 +0100

    Lost last 3 lines

--- directory.rb
+++ directory.rb
@@ -14,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # finally, we print the total number of students
-print "Overall, we have "
-#it’s important that print() doesn’t add new line characters
-print student_count
-puts " great students"
+puts "Overall, we have #{student_count} great students"
