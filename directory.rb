9b5124fd96e84fdd08b482b8285105beca07e71f
jade <jadealvares@hotmail.co.uk>
2017-04-24 15:13:14 +0100

    numbering students

--- directory.rb
+++ directory.rb
@@ -24,8 +24,8 @@ end
   end
 
   def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+1}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
 
   def print_footer(students)
