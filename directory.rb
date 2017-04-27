666085172db5b6b507c0459a4394755a52964e2b
jade <jadealvares@hotmail.co.uk>
2017-04-27 21:06:05 +0100

    Refactoring the code step.10

--- directory.rb
+++ directory.rb
@@ -1,31 +1,8 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_directory(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
 def input_students
   answer = "y"
-  students = []
+  #@students = []
   while answer == "y"
     puts "Hello.  You will be asked to enter the name and other information of a student"
     puts "To finish, just hit return twice"
@@ -53,15 +30,47 @@ def input_students
     puts "Is the input for this student correct? (Type 'y' for yes or 'n' for no)"
     input = gets.strip
         if input == "y"
-          students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height, country_of_birth: country_of_birth}
-          puts "Now we have #{students.count} students"
+          @students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height, country_of_birth: country_of_birth}
+          puts "Now we have #{@students.count} students"
     # get another name from the user
         puts "Input another student? (Type 'y' for yes or type 'n' for no)"
         answer = gets.strip
         end
       end
   # return the array of students
-    return students
+    #return @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
 end
 
 # def print_by_cohort(students)
@@ -74,29 +83,35 @@ end
 #   return by_cohort
 # end
 
-def print_directory(students)
-  count = 1
-  line_width = 50
-    while count <= students.length
-      puts "#{count}: #{students[count-1][:name]}".center(line_width)
-      puts "#{students[count-1][:cohort]}".center(line_width)
-      puts "#{students[count-1][:hobby]}".center(line_width)
-      puts "#{students[count-1][:height]}".center(line_width)
-      puts "#{students[count-1][:country_of_birth]}".center(line_width)
-    count += 1
+# def print_directory(@students)
+#   count = 1
+#   line_width = 50
+#     while count <= students.length
+#       puts "#{count}: #{students[count-1][:name]}".center(line_width)
+#       puts "#{@students[count-1][:cohort]}".center(line_width)
+#       puts "#{@students[count-1][:hobby]}".center(line_width)
+#       puts "#{@students[count-1][:height]}".center(line_width)
+#       puts "#{@students[count-1][:country_of_birth]}".center(line_width)
+#     count += 1
+#   end
+# end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+@students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-  def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-  end
-
-def print_footer(students)
-  if students.count == 1
+def print_footer
+  if @students.count == 1
     puts "Overall, we have 1 great student"
   elsif students.count >= 2
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
