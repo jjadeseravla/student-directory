7298d4eb06e37523976662052b2555be1291b126
jade <jadealvares@hotmail.co.uk>
2017-04-28 11:12:00 +0100

    step.11

--- directory.rb
+++ directory.rb
@@ -51,6 +51,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -66,6 +67,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -113,6 +116,18 @@ def print_footer
   elsif students.count >= 2
     puts "Overall, we have #{@students.count} great students"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 #students = input_students
